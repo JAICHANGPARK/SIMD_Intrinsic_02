001772b57848b4feb7ab948b595fd82682e13a1f
Park JaiChang <parkghoxzs@naver.com>
2016-07-21 17:05:14 +0900

    Initial Commit

---
+++ simd_c_test_02/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simd_c_test_02
+//
+//  Created by PARK JAICHANG on 7/21/16.
+//  Copyright © 2016 JAICHANGPARK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
